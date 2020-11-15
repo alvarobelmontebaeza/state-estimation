515106aabf9c75b8b4edb5802cc26f2612c96ce4
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2020-11-15 16:38:59 +0100

    Changed computation. Now velocities are not obtained and we operate directly with distance traveled by wheels

--- packages/encoder_localization/src/encoder_localization.py
+++ packages/encoder_localization/src/encoder_localization.py
@@ -3,8 +3,8 @@ import numpy as np
 import os
 import rospy
 from duckietown.dtros import DTROS, NodeType, TopicType, DTParam, ParamType
-from duckietown_msgs.msg import Pose2DStamped, Twist2DStamped, WheelEncoderStamped
-from geometry_msgs.msg import TransformStamped
+from duckietown_msgs.msg import WheelEncoderStamped
+from geometry_msgs.msg import TransformStamped, Pose2D
 import tf
 import tf_conversions
 
@@ -16,12 +16,11 @@ class EncoderLocalizationNode(DTROS):
         """
 
         # Initialize the DTROS parent class
-        super(EncoderLocalizationNode, self).__init__(node_name=node_name, node_type=NodeType.PERCEPTION)
+        super(EncoderLocalizationNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
         self.veh_name = os.environ['VEHICLE_NAME']
 
-        # State variables for the robot
-        self.pose = Pose2DStamped(None,0.0,0.0,0.0) # Initial state given arbitrarily
-        self.twist = Twist2DStamped(None,0.0,0.0)
+        # State variable for the robot
+        self.pose = Pose2D(0.0,0.0,0.0) # Initial state given arbitrarily
 
         # Transform that defines robot state
         self.current_state = TransformStamped()
@@ -30,14 +29,12 @@ class EncoderLocalizationNode(DTROS):
         self.current_state.transform.translation.z = 0.0 # We operate in a 2D world
 
         # Values computed using encoder data
-        self.v_left = 0.0
-        self.v_right = 0.0
+        self.d_left = 0.0
+        self.d_right = 0.0
 
         # Variables to account for previous step
         self.prev_left = 0.0
         self.prev_right = 0.0
-        self.last_t_left = 0.0
-        self.last_t_right = 0.0
 
         # To account for first received message
         self.first_message_left = True
@@ -46,8 +43,8 @@ class EncoderLocalizationNode(DTROS):
         self.initial_ticks_right = 0.0
 
         # Get static parameters
-        self._radius = rospy.get_param('/' + self.veh_name + '/kinematics_node/radius', 100)
-        self._baseline = rospy.get_param('/' + self.veh_name + '/kinematics_node/baseline', 100)
+        self._radius = rospy.get_param('/' + self.veh_name + '/kinematics_node/radius', 0.031)
+        self._baseline = rospy.get_param('/' + self.veh_name + '/kinematics_node/baseline', 0.1)
         self._resolution = 135.0
 
         # Subscribing to the wheel encoders
@@ -68,18 +65,15 @@ class EncoderLocalizationNode(DTROS):
         """
         # Retrieve encoder data
         ticks = msg.data
-        curr_time = rospy.get_time()
 
         # Check if it's the first received message and store initial encoder value and time
         if wheel == 'left' and self.first_message_left == True:
             self.first_message_left = False
             self.initial_ticks_left = ticks
-            self.last_t_left = rospy.get_time()
             return
         if wheel == 'right' and self.first_message_right ==True:
             self.first_message_right = False
             self.initial_ticks_right = ticks
-            self.last_t_right = rospy.get_time()
             return
 
         # Compute linear velocity
@@ -87,33 +81,27 @@ class EncoderLocalizationNode(DTROS):
             rel_ticks = ticks - self.initial_ticks_left
             diff_ticks = rel_ticks - self.prev_left
             dist = 2 * np.pi * self._radius * (diff_ticks / self._resolution)
-            dt = curr_time - self.last_t_left
             # Obtain linear velocity of left wheel
-            self.v_left = dist / dt
+            self.d_left = dist
             # Update previous number of ticks
             self.prev_left = rel_ticks
-            self.last_t_left = curr_time 
 
         elif wheel == 'right':
             rel_ticks = ticks - self.initial_ticks_right
             diff_ticks = np.abs(rel_ticks - self.prev_right)
             dist = 2 * np.pi * self._radius * (diff_ticks / self._resolution)
-            dt = curr_time - self.last_t_left
             # Obtain linear velocity of right wheel
-            self.v_right = dist / dt
+            self.d_right = dist
             # Update previous number of ticks
             self.prev_right = rel_ticks
-            self.last_t_right = curr_time
         
         ####### UPDATE STATE ##########
-
-        # Compute linear and angular velocity of the robot
-        self.twist.v = (self.v_left + self.v_right) / 2.0
-        self.twist.omega = (self.v_left - self.v_right) / self._baseline
-        # Compute current position
-        self.pose.x = self.pose.x + (self.twist.v * dt) * np.cos(self.pose.theta)
-        self.pose.y = self.pose.y + (self.twist.v * dt) * np.sin(self.pose.theta)
-        self.pose.theta = self.pose.theta + self.twist.omega * dt
+        d = (self.d_right + self.d_left) / 2.0
+        delta_theta = (self.d_right - self.d_left) / self._baseline
+        # Compute current position estimate
+        self.pose.x = self.pose.x + d * np.cos(self.pose.theta)
+        self.pose.y = self.pose.y + d * np.sin(self.pose.theta)
+        self.pose.theta = self.pose.theta + delta_theta
         
         # Update timestamp of transform message
         self.current_state.header.stamp = msg.header.stamp
