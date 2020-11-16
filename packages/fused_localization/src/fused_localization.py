fe90288bb4d3d2000f1acba295e5923c09eb7243
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2020-11-17 00:32:57 +0100

    Implemented behaviour when receiving estimations from apriltag and encoder

--- packages/fused_localization/src/fused_localization.py
+++ packages/fused_localization/src/fused_localization.py
@@ -2,18 +2,14 @@
 import numpy as np
 import os
 import math
-import cv2
-import yaml
-from dt_apriltags import Detector
 
 import rospy
 from duckietown.dtros import DTROS, NodeType
-from sensor_msgs.msg import CompressedImage, CameraInfo
-from image_geometry import PinholeCameraModel
-from cv_bridge import CvBridge, CvBridgeError
-from geometry_msgs.msg import TransformStamped, Pose2D
+from geometry_msgs.msg import TransformStamped, Pose2D, Vector3
 import tf, tf2_ros
 import tf_conversions
+
+from fused_localization.srv import *
 
 class FusedLocalizationNode(DTROS):
 
@@ -24,240 +20,105 @@ class FusedLocalizationNode(DTROS):
         self.veh = rospy.get_namespace().strip("/")
 
         # State variable for the robot
-        self.pose = Pose2D(0.27,0.0,np.pi) # Initial state given arbitrarily
+        self.fused_pose = Pose2D(0.27,0.0,np.pi) # Initial state given arbitrarily
 
-        # Static transforms
-        # Map -> Baselink. To be computed 
-        self.T_map_baselink = TransformStamped()
-        self.T_map_baselink.header.frame_id = 'map'
-        self.T_map_baselink.child_frame_id = 'at_baselink'
-        # Map -> Apriltag. STATIC
-        self._T_map_apriltag = TransformStamped() 
-        self._T_map_apriltag.header.frame_id = 'map'
-        self._T_map_apriltag.header.stamp = rospy.Time.now()
-        self._T_map_apriltag.child_frame_id = 'apriltag'
-        self._T_map_apriltag.transform.translation.x = 0.0
-        self._T_map_apriltag.transform.translation.y = 0.0
-        self._T_map_apriltag.transform.translation.z = 0.09 # Height of 9 cm
-        q = tf_conversions.transformations.quaternion_from_euler(0.0,0.0,0.0)
-        self._T_map_apriltag.transform.rotation.x = q[0]
-        self._T_map_apriltag.transform.rotation.y = q[1]
-        self._T_map_apriltag.transform.rotation.z = q[2]
-        self._T_map_apriltag.transform.rotation.w = q[3]
-        # Apriltag -> Camera. Computed using apriltag detection
-        self.T_apriltag_camera = TransformStamped()
-        self.T_apriltag_camera.header.frame_id = 'apriltag'
-        self.T_apriltag_camera.child_frame_id = 'camera'
-        # Camera -> Baselink. STATIC
-        self._T_camera_baselink = TransformStamped()
-        self._T_camera_baselink.header.frame_id = 'camera'
-        self._T_camera_baselink.header.stamp = rospy.Time.now()
-        self._T_camera_baselink.child_frame_id = 'at_baselink'
-        self._T_camera_baselink.transform.translation.x = -0.0582
-        self._T_camera_baselink.transform.translation.y = 0.0
-        self._T_camera_baselink.transform.translation.z = -0.110
-        q = tf_conversions.transformations.quaternion_from_euler(0.0,np.deg2rad(-15),0.0)
-        self._T_camera_baselink.transform.rotation.x = q[0]
-        self._T_camera_baselink.transform.rotation.y = q[1]
-        self._T_camera_baselink.transform.rotation.z = q[2]
-        self._T_camera_baselink.transform.rotation.w = q[3]
+        # State estimates from apriltag and encoder
+        self.apriltag_tf = None
+        self.encoder_tf = TransformStamped()
+        self.prev_encoder_tf = TransformStamped() # To obtain distance traveled
 
-        # Transformation Matrices to ease computation
-        R_MA = tf_conversions.transformations.euler_matrix(0.0,0.0,0.0,'rxyz')
-        t_MA = tf_conversions.transformations.translation_matrix(np.array([0.0,0.0,0.09]))
-        self.T_MA = tf_conversions.transformations.concatenate_matrices(t_MA,R_MA)
+        # Fused transformation to publish/broadcast
+        self.fused_pose_transform = TransformStamped()
+        self.fused_pose_transform.header.frame_id = 'map'
+        self.fused_pose_transform.child_frame_id = 'fused_baselink'
 
-        R_CB = tf_conversions.transformations.euler_matrix(0.0,np.deg2rad(-15.0),0.0,'rxyz')
-        t_CB = tf_conversions.transformations.translation_matrix(np.array([-0.0582,0.0,-0.1072]))
-        self.T_CB = tf_conversions.transformations.concatenate_matrices(t_CB,R_CB)
-        
-        # Define rotation matrices to follow axis convention in rviz when using apriltag detection
-        self.T_ApA = tf_conversions.transformations.euler_matrix(np.pi/2.0,0.0, -np.pi/2.0,'rxyz')
-        self.T_CCp = tf_conversions.transformations.euler_matrix(-np.pi/2.0,0.0,-np.pi/2.0,'rzyx')
+        # To account for first apriltag detected and first encoder reading
+        self.first_apriltag = True
+        self.first_encoder = True
 
-        # Load calibration files
-        self.calib_data = self.readYamlFile('/data/config/calibrations/camera_intrinsic/' + self.veh + '.yaml')
-        self.log('Loaded intrinsics calibration file')
-        self.extrinsics = self.readYamlFile('/data/config/calibrations/camera_extrinsic/' + self.veh + '.yaml')
-        self.log('Loaded extrinsics calibration file') 
+        # To account for detected apriltag and choose how to update fused_pose
+        self.apriltag_detected = False
 
-        # Retrieve intrinsic info
-        cam_info = self.setCamInfo(self.calib_data)
-        self.cam_model = PinholeCameraModel()
-        self.cam_model.fromCameraInfo(cam_info)
-        # Initiate maps for rectification
-        self._init_rectify_maps()
+        # Subscribers for Apriltag Localization and Encoder Localization
+        self.apriltag_sub = rospy.Subscriber('~at_baselink_transform', TransformStamped, self.apriltag_cb)
+        self.encoder_sub = rospy.Subscriber('~encoder_baselink_transform', TransformStamped, self.encoder_cb)
 
-        # Create AprilTag detector object
-        self.at_detector = Detector()
-
-        # Create cv_bridge
-        self.bridge = CvBridge()
-
-        # Define subscriber to recieve images
-        self.image_sub = rospy.Subscriber('/' + self.veh+ '/camera_node/image/compressed', CompressedImage, self.callback)
         # Publishers and broadcasters
-        self.pub_robot_pose_tf = rospy.Publisher('~pose_transform' , TransformStamped, queue_size=1)
-        self.static_tf_br = tf2_ros.StaticTransformBroadcaster()
+        self.pub_fused_tf = rospy.Publisher('~fused_baselink_transform' , TransformStamped, queue_size=1)
         self.tfBroadcaster = tf.TransformBroadcaster(queue_size=1)
 
-
-        self.log(node_name + ' initialized and running')
-
-    def callback(self, ros_image):
-        '''
-            MAIN TASK
-            Convert the image from the camera_node to a cv2 image. Then, detects 
-            the apriltags position and retrieves the Rotation and translation to it from the
-            camera frame, and next computes T_map_baselink by concatenating transformations.
-            The resultant transformation is published, as well as all intermedium are broadcasted
-        '''
-        # Convert to cv2 image
-        image = self.readImage(ros_image)
-        # Rectify image
-        image = self.processImage(image)
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        # Extract camera parameters
-        K = np.array(self.cam_model.K).reshape((3,3))
-        cam_params = (K[0,0], K[1,1], K[0,2], K[1,2])
-
-        # Detect apriltags
-        detections = self.at_detector.detect(gray_image, estimate_tag_pose=True, camera_params=cam_params, tag_size=0.065)        
-
-        # If an apriltag is detected, update transformations
-        for tag in detections:
-            # Retrieve rotation and translation from apriltag to camera and homogeneize
-            R_CA = tf_conversions.transformations.identity_matrix()
-            R_CA[:3,:3] = tag.pose_R
-            p = tag.pose_t.T[0]
-            t_CA = tf_conversions.transformations.translation_matrix(np.array([p[0],p[1],p[2],1.0]))
-            # Define homogeneous transformation matrix
-            T_CpAp = tf_conversions.transformations.concatenate_matrices(t_CA, R_CA)
-
-            # Compute apriltag->camera transform
-            self.T_CA = tf_conversions.transformations.concatenate_matrices(self.T_CCp, T_CpAp, self.T_ApA)
-            self.T_AC = tf_conversions.transformations.inverse_matrix(self.T_CA)
-
-            # Compute map->baselink transform
-            self.T_MB = tf_conversions.transformations.concatenate_matrices(
-                self.T_MA,self.T_AC,self.T_CB
-            )
-
-            # Obtain TransformStamped messages
-            self.T_apriltag_camera.header.stamp = ros_image.header.stamp
-            t = tf_conversions.transformations.translation_from_matrix(self.T_AC)
-            self.T_apriltag_camera.transform.translation.x = t[0]
-            self.T_apriltag_camera.transform.translation.y = t[1]
-            self.T_apriltag_camera.transform.translation.z = t[2]
-            q = tf_conversions.transformations.quaternion_from_matrix(self.T_AC)
-            self.T_apriltag_camera.transform.rotation.x = q[0]
-            self.T_apriltag_camera.transform.rotation.y = q[1]
-            self.T_apriltag_camera.transform.rotation.z = q[2]
-            self.T_apriltag_camera.transform.rotation.w = q[3]
-            
-            self.T_map_baselink.header.stamp = ros_image.header.stamp
-            t = tf_conversions.transformations.translation_from_matrix(self.T_MB)
-            self.T_map_baselink.transform.translation.x = t[0]
-            self.T_map_baselink.transform.translation.y = t[1]
-            self.T_map_baselink.transform.translation.z = t[2]
-            q = tf_conversions.transformations.quaternion_from_matrix(self.T_MB)           
-            self.T_map_baselink.transform.rotation.x = q[0]
-            self.T_map_baselink.transform.rotation.y = q[1]
-            self.T_map_baselink.transform.rotation.z = q[2]
-            self.T_map_baselink.transform.rotation.w = q[3]
-
-            # Publish transform map -> baselink
-            self.pub_robot_pose_tf.publish(self.T_map_baselink)
-            # Broadcast all transforms
-            self.static_tf_br.sendTransform(self._T_map_apriltag)
-            self.static_tf_br.sendTransform(self._T_camera_baselink)
-            self.tfBroadcaster.sendTransformMessage(self.T_apriltag_camera)
-
-
-
-    def setCamInfo(self, calib_data):
-        '''
-            Introduces the camera information from the dictionary
-            obtained reading the yaml file to a CameraInfo object
-        ''' 
-        cam_info = CameraInfo()
-        cam_info.width = calib_data['image_width']
-        cam_info.height = calib_data['image_height']
-        cam_info.K = calib_data['camera_matrix']['data']
-        cam_info.D = calib_data['distortion_coefficients']['data']
-        cam_info.R = calib_data['rectification_matrix']['data']
-        cam_info.P = calib_data['projection_matrix']['data']
-        cam_info.distortion_model = calib_data['distortion_model']
-        return cam_info  
-
-
-    def readImage(self, msg_image):
-        """
-            Convert images to OpenCV images
-            Args:
-                msg_image (:obj:`CompressedImage`) the image from the camera node
-            Returns:
-                OpenCV image
-        """
+        # Server client to update encoder estimate
+        rospy.wait_for_service('update_encoder_estimate')
         try:
-            cv_image = self.bridge.compressed_imgmsg_to_cv2(msg_image)
-            return cv_image
-        except CvBridgeError as e:
-            self.log(e)
-            return []
+            self.update_encoder_srv = rospy.ServiceProxy('update_encoder_estimate', CalibratePose)
+        except rospy.ServiceException as e:
+            rospy.logerr('Service call failed: %s'%e)
+        
+        # Notify complete initialization
+        self.log(node_name + ' initialized and running')
     
-    def _init_rectify_maps(self):
-        # Get new optimal camera matrix
-        W = self.cam_model.width
-        H = self.cam_model.height
-        rect_camera_K, _ = cv2.getOptimalNewCameraMatrix(
-            self.cam_model.K,
-            self.cam_model.D,
-            (W,H),
-            1.0
-        )
-        # Initialize rectification maps
-        mapx = np.ndarray(shape=(H,W,1), dtype='float32')
-        mapy = np.ndarray(shape=(H,W,1), dtype='float32')
-        mapx, mapy = cv2.initUndistortRectifyMap(self.cam_model.K, self.cam_model.D,np.eye(3),rect_camera_K,(W,H),cv2.CV_32FC1,mapx,mapy)
-        self.cam_model.K = rect_camera_K
-        self.mapx = mapx
-        self.mapy = mapy       
-    
-    def processImage(self, raw_image, interpolation=cv2.INTER_LINEAR):
-        '''
-        Undistort a provided image using the calibrated camera info
-        Implementation based on: https://github.com/duckietown/dt-core/blob/952ebf205623a2a8317fcb9b922717bd4ea43c98/packages/image_processing/include/image_processing/rectification.py
-        Args:
-            raw_image: A CV image to be rectified
-            interpolation: Type of interpolation. For more accuracy, use another cv2 provided constant
-        Return:
-            Undistorted image
-        '''
-        image_rectified = np.empty_like(raw_image)
-        processed_image = cv2.remap(raw_image,self.mapx,self.mapy,interpolation,image_rectified)
+    def apriltag_cb(self, at_tf):
+        # Convert 3D to 2D pose assuming Z = 0 and set fused pose transform
+        self.fused_pose_transform = at_tf
+        self.fused_pose_transform.transform.translation.z = 0
+        angles = tf_conversions.transformations.euler_from_quaternion(at_tf.transform.rotation)
+        self.fused_pose_transform.transform.rotation = tf_conversions.transformations.quaternion_from_euler(0.0,0.0,angles[2])
 
-        return processed_image
-
-
-    def readYamlFile(self,fname):
-        """
-            Reads the 'fname' yaml file and returns a dictionary with its input.
-
-            You will find the calibration files you need in:
-            `/data/config/calibrations/`
-        """
-        with open(fname, 'r') as in_file:
+        if self.first_apriltag:
+            # Call service and update encoder estimate
             try:
-                yaml_dict = yaml.load(in_file)
-                return yaml_dict
-            except yaml.YAMLError as exc:
-                self.log("YAML syntax error. File: %s fname. Exc: %s"
-                         %(fname, exc), type='fatal')
-                rospy.signal_shutdown()
-                return
+                resp = self.update_encoder_srv(self.fused_pose_transform)
+                self.first_apriltag = False
+            except rospy.ServiceException as e:
+                rospy.logerr('Service call failed: %s'%e)
+        
+        # Publish estimate and broadcast every time an apriltag is detected
+        self.pub_fused_tf.publish(self.fused_pose_transform)
+        self.tfBroadcaster.sendTransformMessage(self.fused_pose_transform)
 
+        # Mark that state has been updated with apriltag
+        self.apriltag_detected = True
+
+
+    def encoder_cb(self, enc_tf):
+        if self.first_encoder:
+            # Get first encoder estimate
+            self.first_encoder = False
+            self.prev_encoder_tf = enc_tf
+            return
+
+        # Update encoder estimation
+        self.prev_encoder_tf = self.encoder_tf
+        self.encoder_tf = enc_tf
+
+        # Check if state needs to be updated using encoder
+        if self.apriltag_detected == True:
+            self.apriltag_detected = False
+            return
+        else:
+            # Obtain displacement measured by encoders
+            diff_x = self.encoder_tf.transform.translation.x - self.prev_encoder_tf.transform.translation.x
+            diff_y = self.encoder_tf.transform.translation.y - self.prev_encoder_tf.transform.translation.y
+            curr_angles = tf_conversions.transformations.euler_from_quaternion(self.encoder_tf.transform.rotation)
+            prev_angles = tf_conversions.transformations.euler_from_quaternion(self.prev_encoder_tf.transform.rotation)
+            diff_theta = curr_angles[2] - prev_angles[2]
+            # Update fused pose
+            self.fused_pose.x += diff_x
+            self.fused_pose.y += diff_y
+            self.fused_pose.theta += diff_theta
+            # Update fused pose transform
+            self.fused_pose_transform.transform.translation.x = self.fused_pose.x
+            self.fused_pose_transform.transform.translation.y = self.fused_pose.y
+            self.fused_pose_transform.transform.translation.z = 0.0
+            q = tf_conversions.transformations.quaternion_from_euler(0.0,0.0,self.fused_pose.theta)
+            self.fused_pose_transform.transform.rotation.x = q[0]
+            self.fused_pose_transform.transform.rotation.y = q[1]
+            self.fused_pose_transform.transform.rotation.z = q[2]
+            self.fused_pose_transform.transform.rotation.w = q[3]
+
+            # Publish estimate and broadcast
+            self.pub_fused_tf.publish(self.fused_pose_transform)
+            self.tfBroadcaster.sendTransformMessage(self.fused_pose_transform)
 
     def onShutdown(self):
         super(FusedLocalizationNode, self).onShutdown()
