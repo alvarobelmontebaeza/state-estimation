e44d1e4cb3638714098fb54fb62a40255ccaa294
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2020-11-22 17:18:21 +0100

    Updated apriltag detector object parameters

--- packages/at_localization/src/at_localization.py
+++ packages/at_localization/src/at_localization.py
@@ -91,7 +91,15 @@ class ATLocalizationNode(DTROS):
         self._init_rectify_maps()
 
         # Create AprilTag detector object
-        self.at_detector = Detector()
+        self.at_detector = Detector(
+            families='tag36h11',
+            nthreads=4,
+            quad_decimate=4.0,
+            quad_sigma=0.0,
+            refine_edges=1,
+            decode_sharpening=0.25,
+            debug=0
+        )
 
         # Create cv_bridge
         self.bridge = CvBridge()
